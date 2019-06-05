007e63ce987ecf31b13746ac0bd7fa6980a33968
Artem Kharytoniuk <artemalive@gmail.com>
2019-06-05 10:17:43 +0200

    Solve 6.1

--- epi_judge_cpp/string_integer_interconversion.cc
+++ epi_judge_cpp/string_integer_interconversion.cc
@@ -4,12 +4,32 @@
 using std::string;
 
 string IntToString(int x) {
-  // TODO - you fill in here.
-  return "";
+    if (x == INT_MIN)
+        return "-2147483648";
+    if (x == 0) 
+        return "0";
+    
+    std::string s;
+    if (x < 0) {
+        s.push_back('-');
+        x = -x;
+    }
+   while (x) {
+        s.push_back(x%10 + '0');
+        x /= 10;
+    }
+    std::reverse(s[0] == '-' ? s.begin() + 1 : s.begin(), s.end());
+    return s;
 }
+
 int StringToInt(const string& s) {
-  // TODO - you fill in here.
-  return 0;
+    int x = 0;
+    int base = s[0] == '-' ? -1 : 1;
+    for (int i = (int)s.length() - 1; i >= (s[0] == '-' ? 1 : 0); i--) {
+        x += base * (s[i] - '0');
+        base *= 10;
+    }
+    return x;
 }
 void Wrapper(int x, const string& s) {
   if (IntToString(x) != s) {
