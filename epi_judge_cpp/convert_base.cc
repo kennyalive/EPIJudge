29180e2fd9e181fd92754c4015d6c0a850a10dda
Artem Kharytoniuk <artemalive@gmail.com>
2019-06-05 17:46:01 +0200

    Solve 6.2

--- epi_judge_cpp/convert_base.cc
+++ epi_judge_cpp/convert_base.cc
@@ -3,8 +3,22 @@
 using std::string;
 
 string ConvertBase(const string& num_as_string, int b1, int b2) {
-  // TODO - you fill in here.
-  return "";
+    bool negative = num_as_string[0] == '-';
+    int x1 = 0;
+    for (int i = (num_as_string[0] == '-' ? 1 : 0); i < (int)num_as_string.length(); i++) {
+        char c = num_as_string[i];
+        x1 = x1*b1 + (c >= 'A' ? c - 'A' + 10 : c - '0');
+    }
+    string result;
+    do {
+        int k = x1 % b2;
+        result += k >= 10 ? (k - 10 + 'A') : (k + '0');
+        x1 /= b2;
+    } while (x1);
+    if (negative)
+        result += '-';
+    std::reverse(result.begin(), result.end());
+    return result;
 }
 
 int main(int argc, char* argv[]) {
