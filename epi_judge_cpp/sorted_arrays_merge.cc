03fb3a2a3c36c08e115a7587be6deb2b145fef43
Artem Kharytoniuk <artemalive@gmail.com>
2019-06-07 14:04:51 +0200

    Solve 10.1

--- epi_judge_cpp/sorted_arrays_merge.cc
+++ epi_judge_cpp/sorted_arrays_merge.cc
@@ -2,8 +2,28 @@
 #include "test_framework/generic_test.h"
 using std::vector;
 vector<int> MergeSortedArrays(const vector<vector<int>>& sorted_arrays) {
-  // TODO - you fill in here.
-  return {};
+     struct Deal_Info {
+       int time;
+       int array_index;
+       int array_pos;
+       bool operator<(const Deal_Info& other) const {
+           return time > other.time;
+       }
+    };
+     std::priority_queue<Deal_Info> heap;
+     for (int i = 0; i < (int)sorted_arrays.size(); i++) {
+         heap.push({sorted_arrays[i][0], i, 1});
+     }
+     std::vector<int> result;
+     while (!heap.empty()) {
+         Deal_Info info = heap.top();
+         heap.pop();
+         result.push_back(info.time);
+         if (info.array_pos < (int)sorted_arrays[info.array_index].size()) {
+             heap.push({sorted_arrays[info.array_index][info.array_pos], info.array_index, ++info.array_pos});
+         }
+     }
+     return result;
 }
 
 int main(int argc, char* argv[]) {
