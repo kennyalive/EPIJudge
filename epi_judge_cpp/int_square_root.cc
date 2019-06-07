913c5ef1763aff717b67746d82871f20ff3fa8b5
Artem Kharytoniuk <artemalive@gmail.com>
2019-06-07 22:24:20 +0200

    Solve 11.4

--- epi_judge_cpp/int_square_root.cc
+++ epi_judge_cpp/int_square_root.cc
@@ -1,8 +1,24 @@
 #include "test_framework/generic_test.h"
 
 int SquareRoot(int k) {
-  // TODO - you fill in here.
-  return 0;
+    int L = 0;
+    int U = std::min(k, 46340);
+
+    int R;
+    while (L <= U) {
+        int M = L + (U - L)/2;
+        if (M*M < k) {
+            L = M + 1;
+            R = M;
+        }
+        else if (M*M == k) {
+            return M;
+        }
+        else { // M*M > k
+            U = M - 1;
+        }
+    }
+    return R;
 }
 
 int main(int argc, char* argv[]) {
