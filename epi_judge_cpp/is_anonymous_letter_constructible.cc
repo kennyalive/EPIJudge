05d2cae5570a6c5a4eb3f20b600bb6fbb353366b
Artem Kharytoniuk <artemalive@gmail.com>
2019-06-08 15:25:28 +0200

    Solve 12.2

--- epi_judge_cpp/is_anonymous_letter_constructible.cc
+++ epi_judge_cpp/is_anonymous_letter_constructible.cc
@@ -4,8 +4,18 @@ using std::string;
 
 bool IsLetterConstructibleFromMagazine(const string& letter_text,
                                        const string& magazine_text) {
-  // TODO - you fill in here.
-  return true;
+    int letter_counts[256] = {};
+    int magazine_counts[256] = {};
+
+    for (auto c : letter_text)
+        letter_counts[c]++;
+    for (auto c : magazine_text)
+        magazine_counts[c]++;
+
+    for (int i = 0; i < 256; i++)
+        if (letter_counts[i] > magazine_counts[i])
+            return false;
+    return true;
 }
 
 int main(int argc, char* argv[]) {
