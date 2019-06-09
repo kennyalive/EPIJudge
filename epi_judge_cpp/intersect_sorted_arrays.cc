b44b9414647c530e7853965911d15ef1dd5136d5
Artem Kharytoniuk <artemalive@gmail.com>
2019-06-09 10:08:54 +0200

    Solve 13.1

--- epi_judge_cpp/intersect_sorted_arrays.cc
+++ epi_judge_cpp/intersect_sorted_arrays.cc
@@ -4,8 +4,26 @@ using std::vector;
 
 vector<int> IntersectTwoSortedArrays(const vector<int>& A,
                                      const vector<int>& B) {
-  // TODO - you fill in here.
-  return {};
+    auto a_iter = A.begin();
+    auto b_iter = B.begin();
+    std::vector<int> result;
+    while (a_iter != A.end() && b_iter != B.end()) {
+        if (*b_iter < *a_iter) {
+            ++b_iter;
+        }
+        else if (*a_iter < *b_iter) {
+            ++a_iter;
+        }
+        else {
+            int value = *a_iter;
+            result.push_back(value);
+            while (a_iter != A.end() && *a_iter == value)
+                ++a_iter;
+            while (b_iter != B.end() && *b_iter == value)
+                ++b_iter;
+        }
+    }
+    return result;
 }
 
 int main(int argc, char* argv[]) {
