58c8df6e953e323ebb10caa5d67c8ed503792c7e
Artem Kharytoniuk <artemalive@gmail.com>
2019-06-13 09:48:28 +0200

    Solve 17.4

--- epi_judge_cpp/three_sum.cc
+++ epi_judge_cpp/three_sum.cc
@@ -3,8 +3,22 @@
 using std::vector;
 
 bool HasThreeSum(vector<int> A, int t) {
-  // TODO - you fill in here.
-  return true;
+    std::sort(A.begin(), A.end());
+    for (int i : A) {
+        int value_to_find = t - i;
+        int left = 0;
+        int right = (int)A.size() - 1;
+        while (left <= right) {
+            int sum = A[left] + A[right];
+            if (sum == value_to_find)
+                return true;
+            if (sum > value_to_find)
+                right--;
+            else // sum < value_to_find
+                left++;
+        }
+    }
+    return false;
 }
 
 int main(int argc, char* argv[]) {
