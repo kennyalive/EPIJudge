a92662a868cf9a73518805a0b24182e4ff100399
Artem Kharytoniuk <artemalive@gmail.com>
2019-06-13 07:56:25 +0200

    Solve 16.2

--- epi_judge_cpp/levenshtein_distance.cc
+++ epi_judge_cpp/levenshtein_distance.cc
@@ -4,9 +4,31 @@
 using std::string;
 using std::vector;
 
+int compute_levenshtein_distance(const std::string& A, int a_idx, const std::string& B, int b_idx, std::vector<int>& distance_cache) {
+    if (a_idx == (int)A.size())
+        return (int)B.size() - b_idx;
+    if (b_idx == (int)B.size())
+        return (int)A.size() - a_idx;
+
+    int cache_index = b_idx*(int)A.size() + a_idx;
+    if (distance_cache[cache_index] != -1)
+        return distance_cache[cache_index];
+
+    if (A[a_idx] == B[b_idx])
+        return compute_levenshtein_distance(A, a_idx+1, B, b_idx+1, distance_cache);
+
+    int replace_current_char = compute_levenshtein_distance(A, a_idx+1, B, b_idx + 1, distance_cache);
+    int delete_current_char = compute_levenshtein_distance(A, a_idx+1, B, b_idx, distance_cache);
+    int insert_new_char = compute_levenshtein_distance(A, a_idx, B, b_idx+1, distance_cache);
+
+    int distance = 1 + std::min(replace_current_char, std::min(delete_current_char, insert_new_char));
+    distance_cache[cache_index] = distance;
+    return distance;
+}
+
 int LevenshteinDistance(const string& A, const string& B) {
-  // TODO - you fill in here.
-  return 0;
+    std::vector<int> distance_cache(A.size()*B.size(), -1); 
+    return compute_levenshtein_distance(A, 0, B, 0, distance_cache);
 }
 
 int main(int argc, char* argv[]) {
