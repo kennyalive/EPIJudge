16f73772f8b48c857a16b822f363743878e5aac2
Artem Kharytoniuk <artemalive@gmail.com>
2019-06-21 13:38:00 +0200

    Solve 17.5

--- epi_judge_cpp/majority_element.cc
+++ epi_judge_cpp/majority_element.cc
@@ -1,13 +1,44 @@
 #include <string>
 #include <vector>
 #include "test_framework/generic_test.h"
+#include <unordered_map>
+
 using std::string;
 using std::vector;
+using std::unordered_map;
 
 string MajoritySearch(vector<string>::const_iterator stream_begin,
                       const vector<string>::const_iterator stream_end) {
-  // TODO - you fill in here.
-  return "";
+    // A)
+    /*
+    unordered_map<string, int> element_count;
+    string result;
+    int count= 0;
+    for (; stream_begin != stream_end; stream_begin++) {
+        const string& e = *stream_begin;
+        int n = ++element_count[e];
+        if (n > count) {
+            count = n;
+            result = e;
+        }
+    }
+    return result;
+    */
+    // B)
+    int count = 0;
+    string candidate;
+    while (stream_begin != stream_end) {
+        if (count == 0)
+            candidate = *stream_begin;
+
+        if (candidate == *stream_begin)
+            ++count;
+        else
+            --count;
+
+        ++stream_begin;
+    }
+    return candidate;
 }
 string MajoritySearchWrapper(const vector<string>& stream) {
   return MajoritySearch(cbegin(stream), cend(stream));
