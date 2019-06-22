34444dab0d36e3e2b3a8e40db3a6d0568683a38d
Artem Kharytoniuk <artemalive@gmail.com>
2019-06-22 12:49:07 +0200

    Solve 12.1

--- epi_judge_cpp/is_string_permutable_to_palindrome.cc
+++ epi_judge_cpp/is_string_permutable_to_palindrome.cc
@@ -1,10 +1,25 @@
 #include <string>
 #include "test_framework/generic_test.h"
+#include <unordered_map>
+using std::unordered_map;
 using std::string;
 
+bool can_form_palindrome(const string& s) {
+	unordered_map<char, int> count;
+	for (char c : s)
+		count[c]++;
+
+	int odd_count = 0;
+	for (const std::pair<char, int>& p : count) {
+        if (p.second%2 != 0)
+            odd_count++;
+    }
+    return odd_count <= 1;
+}
+
+
 bool CanFormPalindrome(const string& s) {
-  // TODO - you fill in here.
-  return true;
+    return can_form_palindrome(s);
 }
 
 int main(int argc, char* argv[]) {
