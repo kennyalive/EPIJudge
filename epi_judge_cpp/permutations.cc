da1f588b16f2413f5b0d9811f13b1bd3c401f3dc
Artem Kharytoniuk <artemalive@gmail.com>
2019-06-22 11:05:44 +0200

    Solve 15.4

--- epi_judge_cpp/permutations.cc
+++ epi_judge_cpp/permutations.cc
@@ -2,9 +2,29 @@
 #include "test_framework/generic_test.h"
 using std::vector;
 
+vector<vector<int>> generate_permutation(const vector<int>& a) {
+	vector<vector<int>> result;
+	for (int i = 0; i < (int)a.size(); i++) {
+		auto a2 = a;
+		a2.erase(a2.begin() + i);
+		
+        auto tail_perms = generate_permutation(a2);
+        if (tail_perms.empty())
+            result.push_back({a[i]});
+        else {
+            for (const auto& perm2 : tail_perms) {
+                vector<int> perm;
+                perm.push_back(a[i]);
+                perm.insert(perm.end(), perm2.begin(), perm2.end());
+                result.push_back(std::move(perm));
+            }
+       }
+	}
+	return result;
+}
+
 vector<vector<int>> Permutations(vector<int> A) {
-  // TODO - you fill in here.
-  return {};
+    return generate_permutation(A);
 }
 
 int main(int argc, char* argv[]) {
