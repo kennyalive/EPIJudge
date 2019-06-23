00b4b42ea7f97816fc1dc4494a776aca209a0ad0
Artem Kharytoniuk <artemalive@gmail.com>
2019-06-23 11:49:55 +0200

    Solve 5.2

--- epi_judge_cpp/int_as_array_increment.cc
+++ epi_judge_cpp/int_as_array_increment.cc
@@ -1,9 +1,25 @@
 #include <vector>
 #include "test_framework/generic_test.h"
 using std::vector;
+
+void increment_arbitrary_integer(vector<int>& number) {
+	reverse(number.begin(), number.end());
+	bool carry = true;
+	for (int i = 0; i < (int)number.size() && carry; i++) {
+		if (++number[i] == 10)
+			number[i] = 0;
+		else
+			carry = false;
+    }
+    if (carry)
+        number.push_back(1);
+    reverse(number.begin(), number.end());
+}
+
 vector<int> PlusOne(vector<int> A) {
-  // TODO - you fill in here.
-  return {};
+    auto result = A;
+    increment_arbitrary_integer(result);
+    return result;
 }
 
 int main(int argc, char* argv[]) {
