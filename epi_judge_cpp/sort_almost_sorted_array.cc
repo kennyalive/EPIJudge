4d77a483af319c48a1d43cbc35bf08dec581f4df
Artem Kharytoniuk <artemalive@gmail.com>
2019-06-23 11:19:02 +0200

    Solve 10.3

--- epi_judge_cpp/sort_almost_sorted_array.cc
+++ epi_judge_cpp/sort_almost_sorted_array.cc
@@ -1,11 +1,37 @@
 #include <vector>
 #include "test_framework/generic_test.h"
 using std::vector;
+
+vector<int> sort_almost_sorted_array(
+	vector<int>::const_iterator stream_begin,
+	const vector<int>::const_iterator stream_end,
+	int k)
+{
+	std::priority_queue<int, vector<int>, std::greater<int>> heap;
+    vector<int> output;
+	int n = 0;
+	while (stream_begin != stream_end && n++ < 2*k+1) {
+		heap.push(*stream_begin++);
+    }
+
+    while (stream_begin != stream_end) {
+        output.push_back(heap.top());
+        heap.pop();
+        heap.push(*stream_begin++);
+    }
+
+    while (!heap.empty()) {
+        output.push_back(heap.top());
+        heap.pop();
+    }
+    return output;
+}
+
+
 vector<int> SortApproximatelySortedData(
     vector<int>::const_iterator sequence_begin,
     const vector<int>::const_iterator& sequence_end, int k) {
-  // TODO - you fill in here.
-  return {};
+    return sort_almost_sorted_array(sequence_begin, sequence_end, k);
 }
 vector<int> SortApproximatelySortedDataWrapper(const vector<int>& sequence,
                                                int k) {
