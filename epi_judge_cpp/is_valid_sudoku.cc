633048f45ad6947327d2e8a896944ebd5123ecf2
Artem Kharytoniuk <artemalive@gmail.com>
2019-06-23 12:33:29 +0200

    Solve 5.17

--- epi_judge_cpp/is_valid_sudoku.cc
+++ epi_judge_cpp/is_valid_sudoku.cc
@@ -1,10 +1,57 @@
 #include <vector>
 #include "test_framework/generic_test.h"
 using std::vector;
+bool is_valid_sudoku(const vector<vector<int>>& assignment) {
+	vector<int> has_value(10);
+
+	// rows
+	for (int i = 0; i < 9; i++) {
+		fill(has_value.begin(), has_value.end(), 0);
+		for (int k = 0; k < 9; k++) {
+            int v = assignment[i][k]; 
+			if (v) {
+				if (has_value[v]) return false;
+				has_value[v] = 1;
+			}
+		}
+	}
+
+	// columns
+	for (int i = 0; i < 9; i++) {
+		fill(has_value.begin(), has_value.end(), 0);
+		for (int k = 0; k < 9; k++) {
+            int v = assignment[k][i]; 
+			if (v) {
+				if (has_value[v]) return false;
+				has_value[v] = 1;
+			}
+		}
+	}
+
+	// grid squares
+	for (int i = 0; i < 3; i++)
+		for (int j = 0; j < 3; j++)
+		{
+			fill(has_value.begin(), has_value.end(), 0);
+			for (int x = 0; x < 3; x++)
+				for (int y = 0; y < 3; y++)
+				{
+					int r = i*3 + x;
+					int c = j*3 + y;
+                    int v = assignment[r][c]; 
+					if (v) {
+						if (has_value[v]) return false;
+						has_value[v] = 1;
+					}
+				}
+        }
+	return true;
+}
+
+
 // Check if a partially filled matrix has any conflicts.
 bool IsValidSudoku(const vector<vector<int>>& partial_assignment) {
-  // TODO - you fill in here.
-  return true;
+    return is_valid_sudoku(partial_assignment);
 }
 
 int main(int argc, char* argv[]) {
