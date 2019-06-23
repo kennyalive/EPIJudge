e08b17249c5fade6bef41182897697b1f86f998e
Artem Kharytoniuk <artemalive@gmail.com>
2019-06-23 20:44:48 +0200

    Solve 4.8

--- epi_judge_cpp/reverse_digits.cc
+++ epi_judge_cpp/reverse_digits.cc
@@ -1,7 +1,18 @@
 #include "test_framework/generic_test.h"
 long long Reverse(int x) {
-  // TODO - you fill in here.
-  return 0;
+    bool sign = x < 0;
+    long long k = x;
+    if (sign)
+        k = -k;
+
+    long long reversed = 0;
+    while (k) {
+        reversed = reversed*10 + k%10;
+        k /= 10;
+    }
+    if (sign)
+        reversed = -reversed;
+    return reversed;
 }
 
 int main(int argc, char* argv[]) {
