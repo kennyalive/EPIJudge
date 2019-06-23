10bee86d0f6268525faad8706626b3d672cd1f15
Artem Kharytoniuk <artemalive@gmail.com>
2019-06-23 17:17:37 +0200

    Solve 8.2

--- epi_judge_cpp/evaluate_rpn.cc
+++ epi_judge_cpp/evaluate_rpn.cc
@@ -1,9 +1,48 @@
 #include <string>
 #include "test_framework/generic_test.h"
+#include <stack>
+using std::stack;
 using std::string;
 int Evaluate(const string& expression) {
-  // TODO - you fill in here.
-  return 0;
+    stack<int> values;
+    auto it = expression.begin();
+    while (it != expression.end()) {
+        if ((*it == '+' || *it == '-' || *it == '*' || *it == '/') &&
+                (it+1 == expression.end() || *(it+1) == ',')) {
+            int arg1 = values.top();
+            values.pop();
+            int arg0 = values.top();
+            values.pop();
+            if (*it == '+')
+                values.push(arg0 + arg1);
+            else if (*it == '-')
+                values.push(arg0 - arg1);
+            else if (*it == '*')
+                values.push(arg0 * arg1);
+            else
+                values.push(arg0 / arg1);
+            ++it;
+        }
+        else if (*it == ',') {
+            ++it;
+        }
+        else {
+            int value = 0;
+            bool sign = false;
+            if (*it == '-') {
+                sign = true;
+                ++it;
+            }
+            while (it != expression.end() && isdigit(*it)) {
+                value = value*10 + (*it - '0');
+                ++it;
+            }
+            if (sign)
+                value = -value;
+            values.push(value);
+        }
+    }
+    return values.top();
 }
 
 int main(int argc, char* argv[]) {
