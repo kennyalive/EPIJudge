0ce092dff94350e7ad78f61cfd14c71364f984d2
Artem Kharytoniuk <artemalive@gmail.com>
2019-06-23 12:44:38 +0200

    Solve 9.2

--- epi_judge_cpp/is_tree_symmetric.cc
+++ epi_judge_cpp/is_tree_symmetric.cc
@@ -1,9 +1,21 @@
 #include "binary_tree_node.h"
 #include "test_framework/generic_test.h"
 
+bool is_subtrees_symmetric(const BinaryTreeNode<int>* a, const BinaryTreeNode<int>* b) {
+    if (a == nullptr && b == nullptr)
+        return true;
+
+    if (a == nullptr || b == nullptr || a->data != b->data)
+        return false;
+
+    return is_subtrees_symmetric(a->left.get(), b->right.get()) && is_subtrees_symmetric(a->right.get(), b->left.get());
+}
+
 bool IsSymmetric(const unique_ptr<BinaryTreeNode<int>>& tree) {
-  // TODO - you fill in here.
-  return true;
+    if (!tree)
+        return true;
+
+    return is_subtrees_symmetric(tree->left.get(), tree->right.get());
 }
 
 int main(int argc, char* argv[]) {
